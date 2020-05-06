2972d30ca8786c65a3264365107f0e155ee52658
Ismail Farooq <ismaeelfarooq53@gmail.com>
2020-05-06 22:29:08 +0500

    Scraping quotes off of quotestoscrape using scrapy and splash + dockers

---
+++ quotes/quotes/spiders/quotes_scrape.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+from scrapy_splash import SplashRequest
+
+
+class QuotesScrapeSpider(scrapy.Spider):
+    name = 'quotes_scrape'
+    allowed_domains = ['quotes.toscrape.com/js/']
+    
+
+    script = '''
+        function main(splash, args)
+            splash.private_mode_enabled = false
+            url = args.url
+            assert(splash:go(url))
+            assert(splash:wait(1))
+            return splash:html()
+        end
+    '''    
+
+    def start_requests(self):
+        print("\nHELLOOO\n")
+        yield SplashRequest(url="http://quotes.toscrape.com/js/", callback=self.parse, endpoint="execute", args={
+            'lua_source': self.script
+        })
+
+    def parse(self, response):
+        print("\nPSSSSSTTTT\n")
+        print(response)
